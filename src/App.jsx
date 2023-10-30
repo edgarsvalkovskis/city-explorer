81f29912c96b62a93ca67a2cd8b148665ed4b828
EDGARS VALKOVSKIS <EDATBIZ@GMAIL.COM>
2023-10-30 16:03:49 +0000

    end

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import axios from "axios";
+import { useState } from "react";
+
+const API_KEY = import.meta.env.VITE_API_KEY;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [location, setLocation] = useState({});
+  const [search, setSearch] = useState("");
+  const [lat, setLat] = useState("");
+  const [lon, setLon] = useState("");
+  const [staticmap, setImg] = useState("");
+  function handleChange(event) {
+    setSearch(event.target.value);
+  }
+
+
+  https://maps.locationiq.com/v1/staticmap?key=<YOUR_ACCESS_TOKEN>&center=40.7128,-74.0060&zoom=16&size=600x400&format=png&maptype=roadmap
+
+  async function getLocation(event) {
+    event.preventDefault();
+
+    // the API url we are going to make a request to
+    const API = `https://eu1.locationiq.com/v1/search?q=${search}&key=${API_KEY}&format=json`;
+
+    // make the GET request
+    const res = await axios.get(API);
+
+    // set location to be our response
+    console.log(res.data[0]);
+    setLocation(res.data[0]);
+    setLat(res.data[0].lat);
+    setLon(res.data[0].lon);
+    setImg(res.data[0].staticmap);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>APIs</h1>
+      <form onSubmit={getLocation}>
+        <input onChange={handleChange} placeholder="Location" />
+        <button>Explore!</button>
+      </form>
+
+      <h3>Location: {location.display_name}</h3>
+      <h3>Latitude: {lat}</h3>
+      <h3>Longitude: {lon}</h3>
+      <h2>Static Map {staticmap}</h2>
+
+      {/* information about the location saved in state */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
